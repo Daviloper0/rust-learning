eebc383bea43b008d959ac779031d2cbb6aa4210
Davi de Almeida Guimarães <odaviloper@gmail.com>
2025-07-03 19:59:16 -0300

    Initial commit

---
+++ ex01-helloworld/main.rs
@@ -0,0 +1,3 @@
+fn main() { // Primeiro código que roda em todo programa RUST (como em C)
+    println!("Hello, world!"); // Isso é um macro, por que? Eu não faço ideia (eu sei que é por causa do !), mas por que não existe um println em rust?
+}
